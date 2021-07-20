8dacfec90492d2c3a603df6f68921e5a1e70ce53
halka asahina <halka.rjch@gmail.com>
2021-07-20 19:26:32 +0900

    revert

--- unicorn.rb
+++
@@ -1,9 +0,0 @@
-@dir = "/home/nginx/metar"
-worker_processes 2
-working_directory @dir
-preload_app true
-timeout 30
-listen "#{@dir}/tmp/metar.sock", :backlog => 64
-pid "#{@dir}/tmp/pids/unicorn.pid"
-stderr_path "#{@dir}/log/unicorn.stderr.log"
-stdout_path "#{@dir}/log/unicorn.stdout.log"
